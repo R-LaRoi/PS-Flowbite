6998ac8b998792e36258f6d43324e3ff6365cd80
R. LaRoi <are@raewhoo.com>
2024-04-11 18:31:40 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+        <script
+          src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"
+          async
+        ></script>
       </body>
     </html>
   );
